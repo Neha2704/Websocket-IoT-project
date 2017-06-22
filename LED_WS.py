21d20256a7b185b3ead2bda60998bcccb041914d
Neha2704 <nsrivastava.g@gmail.com>
2017-06-22 10:44:41 +0530

    Update LED_WS.py

--- LED_WS.py
+++ LED_WS.py
@@ -15,3 +15,9 @@ class LedHandler(tornando.websocket.WebSocketHandler):
     def on_message(self, message):
         print "Message received: {}".format(message)
         self.write_message("Message recieved")
+if __name__ == "__main":
+    tornado.optins.parse_command_line()
+    app = tornado.web.Apllication(handlers=[(r"/", LedHandler)])
+    server = tornado.httpserver.HTTPServer(appp)
+    server.listen(8000)
+    tornado.ioloop.IOLoop.instance().start()
