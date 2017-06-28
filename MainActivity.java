a577eab70382a28581e2ebf347a2dcb6b6e51b54
Neha2704 <nsrivastava.g@gmail.com>
2017-06-28 11:11:24 +0530

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -6,7 +6,64 @@ public class MainActivity extends AppCompatActivity {
     
     private final class EchoWebSocketListener extends WebSocketListener {
         @Override
-        public void onOpen (WebSocket webSocket,Response response) {
-            webSocket.send ("Hello World!");
-            webSocket.send ("What's up");
-            webSocket.send ("What's up");
+        public void onOpen (WebSocket websocket,Response response) {
+            websocket.send ("Hello World!");
+            websocket.send ("What's up");
+            websocket.send (ByteString.decodeHex("Try it!"));
+            websocket.close (NORMAL_CLOSURE_STATUS, "Goodbye!");
+        }
+        
+        @Override
+        public void onMessage (WebSocket websocket, String text) {
+            output ("Recieved :" + text);
+        }
+        
+        @Override
+        public void onMessage (WebSocket websocket, ByteString bytes) {
+            output ("Received bytes :" + bytes.hex());
+        }
+        
+        @Override
+        public void onClosing (WebSocket websocket, int code, String reason) {
+            websocket.close (NORMAL_CLOSURE_STATUS, null);
+            output ("Closing " + code + " / " + reason);
+        }
+        
+        @Override
+        public void onFailure (WebSocket websocket, Throwable t, Response response) {
+            output ("Error: " + t.getMessage());
+        }
+    }
+        
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        start = (Button) findViewById (R.id.start);
+        output = (TextView) findViewById (R.id.output);
+        client = new OkHttpClient();
+        
+        start.onClickListener (new View.OnCLickListener() {
+            @Override
+            public void onClick (View view) {
+                start();
+            }
+        });
+    }
+    
+    private void start() {
+        Request request = new Request.Builder().url("").build();
+        EchoWebSocketListener listener = new EchoWebSocketListener();
+        WebSocket ws = client.newWebSocket (request, listener);
+        
+        client.dispatcher().executorService().shutdown();
+    
+    private void output (final string txt) {
+        runOnUiThread (new Runnable () {
+            @Override
+            public void run () {
+                output.setText(output.getText().toString() + "\n\n" + txt);
+            }
+        });
+    }
+}        
